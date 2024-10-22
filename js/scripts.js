8c4740091c90ed5aac230e1eba27e6c47dafa004
ccalash98 <cristtofher.ccalash@gmail.com>
2024-10-22 16:26:05 -0500

    sinc

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
+$(window).on('load', function() {
+    // Ocultar el loader una vez que la página se ha cargado completamente
+    $(".loader_bg").fadeOut(); // Esto ocultará el loader gradualmente
+});
+
 $(function () {
     var bingo = {
         selectedNumbers: [],
         generateRandom: function () {
             var min = 1;
-            var max = 89;
+            var max = 75; // Usamos números del 1 al 75
             return Math.floor(Math.random() * (max - min + 1)) + min;
         },
         generateNextRandom: function () {
-            if (bingo.selectedNumbers.length > 88) {
-                alert("All numbers exhausted");
+            if (bingo.selectedNumbers.length >= 24) {
+                alert("Todos los números han sido generados");
                 return 0;
             }
             var random = bingo.generateRandom();
@@ -20,24 +25,19 @@ $(function () {
         }
     };
 
-    $('td').each(function () {
-        var concatClass = this.cellIndex + "" + this.parentNode.rowIndex;
-        var numberString = parseInt(concatClass, 10).toString();
-        $(this).addClass("cell" + numberString).text(numberString);
-    });
+    // Llenar las 24 celdas con números aleatorios del 1 al 75, excepto la celda del medio
+    var cells = $('td').not('#middle'); // Excluir la celda del medio
+    var index = 0;
 
     $('#btnGenerate').click(function () {
-        var random = bingo.generateNextRandom().toString();
-        $('.bigNumberDisplay span').text(random);
-        $('td.cell' + random).addClass('selected');
-    });
-
-    window.onbeforeunload = function (e) {
-        e = e || window.event;
-        var returnString = 'Are you sure?';
-        if (e) {
-            e.returnValue = returnString;
+        if (index >= 24) {
+            alert("Se han llenado todas las celdas.");
+            return;
         }
-        return returnString;
-    };
+
+        var random = bingo.generateNextRandom().toString();
+        $(cells[index]).text(random);
+        $(cells[index]).addClass('selected');
+        index++;
+    });
 });
